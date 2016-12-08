4262a5abec43ae4490e7f87e96ade694f2ac6faf
Avi Aryan <avi.aryan123@gmail.com>
2016-12-08 21:35:08 +0530

    use graphview to show graph in detail view
    PS - Master - Detail stacking done correctly, see Manifest

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,39 @@
+package com.udacity.stockhawk.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.udacity.stockhawk.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.graph) GraphView graph;
+    @BindView(R.id.stock_name) TextView stockNameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ButterKnife.bind(this);
+
+        stockNameView.setText(getIntent().getStringExtra("symbol"));
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+}
--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.stockhawk.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -45,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onClick(String symbol) {
         Timber.d("Symbol clicked: %s", symbol);
+        Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+        detailIntent.putExtra("symbol", symbol);
+        MainActivity.this.startActivity(detailIntent);
     }
 
     @Override
