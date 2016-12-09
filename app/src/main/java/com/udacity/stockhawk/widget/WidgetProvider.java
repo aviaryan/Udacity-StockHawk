639125128bf26e5e57c1ea59d5f1884e21ce5713
aviaryan <avi.aryan123@gmail.com>
2016-12-09 15:31:28 +0530

    created basic widget. currently doesn't show in widgets list because of lack of config activity I think

---
+++ app/src/main/java/com/udacity/stockhawk/widget/WidgetProvider.java
@@ -0,0 +1,21 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by aviaryan on 9/12/16.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
